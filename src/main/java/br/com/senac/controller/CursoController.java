0b2ad1b9c3e14f1860186d326c80b50bbe9b3e4a
Haony Duarte Porto Vieira <haonyduarte@gmail.com>
2019-04-15 21:46:40 -0300

    Aula 4 até o slide 8.

---
+++ src/main/java/br/com/senac/controller/CursoController.java
@@ -0,0 +1,26 @@
+package br.com.senac.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.senac.servico.CategoriaService;
+import br.com.senac.servico.CursoService;
+
+@Controller
+public class CursoController {
+
+	@Autowired
+	private CursoService cursoService;
+	
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@GetMapping("/listaCurso")
+	public ModelAndView listaCursos() {
+		ModelAndView mv = new ModelAndView("curso/paginaCursos");
+		mv.addObject("cursos", cursoService.listaCursos());
+		return mv;
+	}
+}

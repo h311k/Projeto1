c5d31912155c26d29e22a2f75980b21c28a644b5
36118052017.2N <36118052017.2N@36DE196167.senacrjedu.local>
2019-04-16 21:22:20 -0300

    Aula 4 completa.

--- src/main/java/br/com/senac/controller/CursoController.java
+++ src/main/java/br/com/senac/controller/CursoController.java
@@ -3,12 +3,18 @@ package br.com.senac.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.senac.dominio.Curso;
 import br.com.senac.servico.CategoriaService;
 import br.com.senac.servico.CursoService;
+import br.com.senac.servico.exception.ObjectNotFoundException;
 
 @Controller
+@RequestMapping("/curso")
 public class CursoController {
 
 	@Autowired
@@ -23,4 +29,40 @@ public class CursoController {
 		mv.addObject("cursos", cursoService.listaCursos());
 		return mv;
 	}
+	
+	@GetMapping("/adicionarCurso")
+	public ModelAndView add() {
+		ModelAndView mv = new ModelAndView("curso/paginaAdicionarCurso");
+		mv.addObject("curso", new Curso());
+		mv.addObject("categorias", categoriaService.listaCategoria());
+		return mv;
+	}
+	
+	@PostMapping("/salvarCurso")
+	public ModelAndView inserir(Curso curso) {
+		cursoService.inserir(curso);
+		return listaCursos();
+	}
+	
+	@GetMapping("/excluirCurso/{id}")
+	public ModelAndView delete(@PathVariable("id") Integer id) {
+		cursoService.excluir(id);
+		return listaCursos();
+	}
+	
+	@GetMapping("/alterarCurso/{id}")
+	public ModelAndView alterar(@PathVariable("id") Integer id) throws ObjectNotFoundException {
+		ModelAndView mv = new ModelAndView("curso/paginaAlterar");
+		mv.addObject("curso", cursoService.buscar(id));
+		mv.addObject("categorias", categoriaService.listaCategoria());
+		return mv;
+	}
+	
+	@PostMapping("/alterarCurso")
+	public ModelAndView alterar(Curso curso) throws ObjectNotFoundException {
+		cursoService.alterar(curso);
+		return listaCursos();
+	}
+	
+	
 }
